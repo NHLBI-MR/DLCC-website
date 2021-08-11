d96a0ddda0c2ea085cebef7dd97b9fcbef15222c
Hui Xue <hui.xue@nih.gov>
2021-08-11 19:00:27 -0400

    Deployed e171058 with MkDocs version: 1.2.1

--- setup/install_windows_dependencies.bat
+++ setup/install_windows_dependencies.bat
@@ -2,6 +2,6 @@ REM Hui Xue
 REM 7/31/2021
 REM for the deep leanring crash course
 
-pip install -U pip setuptools testresources
-pip install Cython matplotlib numpy opencv_python pydicom Pillow pyxb scikit-image scikit-learn scipy sympy tk-tools junitparser pandas seaborn pynvml xsdata onnx onnxruntime six future tqdm wandb
-pip install torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio===0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
+pip3 install -U pip setuptools testresources
+pip3 install Cython matplotlib numpy opencv_python pydicom Pillow pyxb scikit-image scikit-learn scipy sympy tk-tools junitparser pandas seaborn pynvml xsdata onnx onnxruntime six future tqdm wandb
+pip3 install torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio===0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
